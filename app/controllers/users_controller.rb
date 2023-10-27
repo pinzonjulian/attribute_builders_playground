2878be6b5b80f6ab0d96f0f0feed0a3af9077097
Julián Pinzón Eslava <julian@julianpinzon.com>
2023-10-27 21:43:32 +1100

    generalise style type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,18 @@ class UsersController < ApplicationController
   def create
     session[:to_print] = params
     @user = User.new(user_params)
+    @style_type = case request.referrer
+                  when /material/
+                    "material"
+                  when "shoelace"
+                    "shoelace"
+                  else
+                    raise "unsupported"
+                  end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :age, :email, :password, :onboarded, :active)
   end
