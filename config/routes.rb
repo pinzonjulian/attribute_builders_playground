72a855c618454357a18fff59d95701592f20ff3f
Julián Pinzón Eslava <julian@julianpinzon.com>
2023-10-27 21:38:04 +1100

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   get 'users/create'
   get 'material', to: "material#show"
-  get 'material/form'
-  get 'material/form_source'
-  get 'material/form_code'
-  get 'material/form_builder'
-  get 'material/params'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
