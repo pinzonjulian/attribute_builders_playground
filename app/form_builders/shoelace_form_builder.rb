d5d763f47f4b4c2c592c9b68b39e5fee4f776a41
Julián Pinzón Eslava <julian@julianpinzon.com>
2023-11-08 01:20:12 +1100

    Update shoelace formbuilder

--- app/form_builders/shoelace_form_builder.rb
+++ app/form_builders/shoelace_form_builder.rb
@@ -1,7 +1,13 @@
-
 class ShoelaceFormBuilder < ApplicationFormBuilder
-
-  text_fields = %i( text_field number_field color_field email_field search_field tel_field password_field url_field )
+  text_fields = %i(
+    text_field
+    email_field
+    number_field
+    password_field
+    search_field
+    tel_field
+    url_field
+ )
   # e.g. Text Field
   # def text_field(method, options = {})
   #   build_text_field(method, options, __method__)
@@ -12,23 +18,49 @@ class ShoelaceFormBuilder < ApplicationFormBuilder
     end
   end
 
-  def switch(method, options = { }, checked_value = nil, unchecked_value = nil)
-    attribute_builder = checkbox_attribute_builder(method, options, checked_value)
+  def switch(method, options = {}, checked_value = "on", unchecked_value = "off")
+    attribute_builder = ActionView::Helpers::AttributeBuilders::CheckBox.new(object_name, method, @template, checked_value, options)
     html_attributes = attribute_builder.html_attributes
 
-    @template.content_tag("sl-switch", nil, html_attributes)
+    @template.capture do
+      # copied from Rails' check_box tag.
+      # Can't use hidden_field because it includes more stuff we don't need.
+      hidden_options = html_attributes.slice("name", "disabled", "form").merge!("type" => "hidden", "value" => unchecked_value, "autocomplete" => "off")
+      @template.concat @template.tag(:input, hidden_options)
+      @template.concat @template.content_tag("sl-switch", nil, html_attributes)
+    end
   end
 
-  def check_box(method, options = { }, checked_value = nil, unchecked_value = nil)
+  def check_box(method, options = {}, checked_value = "on", unchecked_value = "off")
     attribute_builder = checkbox_attribute_builder(method, options, checked_value)
     html_attributes = attribute_builder.html_attributes
-
-    @template.content_tag("sl-checkbox", nil, html_attributes)
+    @template.capture do
+      # copied from Rails' check_box tag.
+      # Can't use hidden_field because it includes more stuff we don't need.
+      hidden_options = html_attributes.slice("name", "disabled", "form").merge!("type" => "hidden", "value" => unchecked_value, "autocomplete" => "off")
+      @template.concat @template.tag(:input, hidden_options)
+      @template.concat @template.content_tag("sl-checkbox", nil, html_attributes)
+    end
   end
 
-  # incomplete
+  def slider(method, options = {})
+    attribute_builder = range_field_attribute_builder(method, objectify_options(options))
+    html_attributes = attribute_builder.html_attributes
+
+    @template.content_tag("sl-range", nil, html_attributes)
+  end
+
+  def radio_button(method, tag_value, options = {})
+    attribute_builder = radio_button_attribute_builder(method, tag_value, options)
+    html_attributes = attribute_builder.html_attributes
+
+    @template.content_tag("md-radio", nil, html_attributes)
+  end
+
   def button(value = nil, options = {}, &block)
-    @template.content_tag("sl-button", value, options.merge!(variant: :primary, type: :submit))
+    attribute_builder = button_attribute_builder(value, options)
+
+    @template.content_tag("sl-button", attribute_builder.value, attribute_builder.html_attributes.merge!(variant: :primary, type: :submit))
   end
 
   private
@@ -45,7 +77,7 @@ class ShoelaceFormBuilder < ApplicationFormBuilder
       html_attributes.merge!("class": "field-with-error", "help-text" => object.errors.where(method).map(&:full_message))
     end
 
-    @template.content_tag("sl-input", nil, html_attributes) do
+    @template.content_tag("sl-input", html_attributes) do
       if icon.present?
         @template.content_tag(
           "sl-icon",
